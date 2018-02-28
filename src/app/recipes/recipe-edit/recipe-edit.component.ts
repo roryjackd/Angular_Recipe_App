855fed012e10211a7a1bf5d662b1f7c5ca8fc083
roryjackd <roryjackd@yahoo.com>
2018-02-28 15:19:07 -0700

    Added delete and clear functionality

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 import { RecipeService } from '../recipe.service';
-import { Recipe } from '../recipe.model';
 
 
 @Component({
@@ -16,7 +15,9 @@ export class RecipeEditComponent implements OnInit {
   recipeForm: FormGroup;
 
   constructor(private route: ActivatedRoute, 
-    private recipeService: RecipeService) { }
+              private recipeService: RecipeService,
+              private router: Router) { 
+  }
 
   ngOnInit() {
     this.route.params
@@ -40,6 +41,7 @@ export class RecipeEditComponent implements OnInit {
     } else {
       this.recipeService.addRecipe(this.recipeForm.value);
     }
+    this.onCancel();
   }
 
   onAddIngredient(){
@@ -52,6 +54,10 @@ export class RecipeEditComponent implements OnInit {
           ])
       })
     );
+  }
+
+  onCancel() {
+    this.router.navigate(['../'], {relativeTo: this.route});
   }
 
   private initForm() {
