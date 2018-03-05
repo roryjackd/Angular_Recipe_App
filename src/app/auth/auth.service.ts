96349d9a472a1f67bcc7d7edb47af30131b67620
roryjackd <roryjackd@yahoo.com>
2018-03-04 22:59:59 -0700

    Implemented token

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,13 @@
+import { Router } from '@angular/router';
 import * as firebase from 'firebase';
+import { Injectable } from '@angular/core';
 
+@Injectable()
 export class AuthService {
+  token: string;
+
+  constructor(private router: Router) {}
+
   signupUser(email: string, password: string) {
     firebase.auth().createUserWithEmailAndPassword(email, password)
       .catch(
@@ -11,10 +18,33 @@ export class AuthService {
   signinUser(email: string, password: string) {
     firebase.auth().signInWithEmailAndPassword(email, password)
       .then(
-          response => console.log(response)
-        )
+        response => {
+          this.router.navigate(['/']);
+          firebase.auth().currentUser.getToken()
+            .then(
+              (token: string) => this.token = token
+            )
+        }
+      )
       .catch(
-          error => console.log(error)
-        );
+        error => console.log(error)
+      );
+  }
+
+  logout() {
+    firebase.auth().signOut();
+    this.token = null;
+  }
+
+  getToken() {
+    firebase.auth().currentUser.getToken()
+      .then(
+        (token: string) => this.token = token
+      );
+    return this.token;
+  }
+
+  isAuthenticated() {
+    return this.token != null;
   }
 }
