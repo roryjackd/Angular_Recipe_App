8b9c510341db5eaf0452502ea76f2b48c51dc7af
roryjackd <roryjackd@yahoo.com>
2018-03-14 18:55:02 -0600

    Implement auth token

--- src/app/auth/store/auth.reducers.ts
+++ src/app/auth/store/auth.reducers.ts
@@ -20,10 +20,15 @@ export function authReducer(state = initialState, action: AuthActions.AuthAction
       };
     case (AuthActions.LOGOUT):
       return {
-          ...state,
-          token: null,
-          authenticated: false
-        };
+        ...state,
+        token: null,
+        authenticated: false
+      };
+    case (AuthActions.SET_TOKEN):
+      return {
+        ...state,
+        token: action.payload
+      };    
     default:    
       return state;
   }
